794292204782de127a021d872c3f7cd38d27ae9f
Jannis Lindenberg <jlindenberg@stud.hs-bremen.de>
2015-06-09 21:12:20 +0200

    vorläufig neues Menu + Intro

--- js/menu.js
+++ js/menu.js
@@ -13,3 +13,25 @@ $(document).ready(function(){
         $("#HowToPic").fadeOut(1500);
     });
 });
+
+$(document).ready(function(){
+
+    $("#Intro").click(function(){
+
+        $("#video").fadeIn(1500);
+        $("#Black").fadeIn(1500);
+    });
+
+});
+
+
+$(document).ready(function(){
+	$("#video").bind('ended', function(){
+
+		$("#video").fadeOut(2000);
+		$("#Black").fadeOut(2000);
+
+	});
+});
+
+
