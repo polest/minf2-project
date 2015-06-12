e9ee1e88658160779239acb78511fe1544895e51
Jannis Lindenberg <jlindenberg@stud.hs-bremen.de>
2015-06-12 13:58:56 +0200

    incl. LevelwechselMenu (noch nicht implementiert

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ game.state.add('Game', MainGame.Game);
 game.state.add('Menu', MainGame.Menu);
 game.state.add('LevelMenu1', MainGame.LevelMenu1);
 game.state.add('Pause', MainGame.Pause);
+game.state.add('Levelwechsel', MainGame.Levelwechsel);
 
 //game.state.start("Boot",true,false, 1);
 //game.state.start('Boot');
